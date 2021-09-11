db331b8d2c6709328e194d2f5ee2f1ba6e6d6443
Vinicius Oliveira <vinicius.oliveira148571@gmail.com>
2021-09-11 03:46:42 -0300

    feat: schedule entity

---
+++ backend/src/entities/Schedule.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Office } from "./Office";
+import { User } from "./User";
+
+@Entity("schedules")
+class Schedule{
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    user_id:number;
+    @JoinColumn({name: "user_id"})
+    @ManyToOne( ()=> User)
+    user: User;
+
+    @Column()
+    office_id:number;
+    @JoinColumn({name: "office_id"})
+    @ManyToOne( () => Office)
+    office: Office
+
+    @Column()
+    schedule_date:Date;
+}
+
+export {Schedule}
