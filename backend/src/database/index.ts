eb8ac6637e42ca483d0154afa2d0865c97d56535
Vinicius Oliveira <vinicius.oliveira148571@gmail.com>
2021-09-16 12:20:27 -0300

    feat: config dotenv

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,3 +1,22 @@
 import {createConnection} from "typeorm";
 
-createConnection();
+require('dotenv').config();
+
+console.log(process.env.DB_HOST)
+
+createConnection({
+    "type": "mysql",
+    "host": process.env.DB_HOST,
+    "port": 3306,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": "bd_ProjetoFCamara",
+    "entities": ["src/entities/*.ts"],
+    "logging": true,
+    "synchronize": true,
+    "migrations": ["src/database/migrations/*.ts"],
+    "cli":{
+        "entitiesDir": "src/entities",
+        "migrationsDir": "src/database/migrations"
+    }
+})
